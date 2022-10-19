23a1e5d11a8461541dda0092d9a50352dd7404d0
林学文 <826982262@qq.com>
2022-10-19 15:49:19 +0800

    初步测试

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,21 @@ router.get('/api/doswagger/checkfiles', async (ctx, next) => {
   await ApiService.checkConfigFileService(ctx,next)
 })
 // 配置文件执行
+router.post('/api/doswagger/execConfig', async (ctx, next) => {
+  await ApiService.execFile(ctx,next)
+})
+
 //配置文件修改
-// 配置文件删除
+router.post('/api/doswagger/deleteFile', async (ctx, next) => {
+  await ApiService.deleteFile(ctx,next)
+})
+
+
+// 配置文件查看
+router.post('/api/doswagger/lookupConfig', async (ctx, next) => {
+  await ApiService.lookupConfig(ctx,next)
+})
+
 
 // 配置文件上传
 router.post('/api/doswagger/upload', async (ctx, next) => {
--- service/swaggerService.js
+++ service/swaggerService.js
@@ -1,7 +1,6 @@
 let co = require('co');//异步控制器
 const fs = require("fs")
 const path = require('path')
-
 const {WORKDIR,SWAGGERJSONFILE} = require('../config/config.default.js');
 const {getCheckSum,checkConfigFile,checkSumEqueir} = require('../utils/fileUtils')
 
@@ -34,8 +33,6 @@ let checkConfigFileService = async (ctx, next) => {
  })
     ctx.body = res
 } 
-
-
 // 文件上传
 let uploadFile= async (ctx, next) => {
     const file = ctx.request.files.file; // 获取上传文件
@@ -45,13 +42,61 @@ let uploadFile= async (ctx, next) => {
         ctx.throw({success: false,code: 502, message: "文件类型错误"})
     }
     const reader = fs.createReadStream(file.filepath);
-    let filePath = path.join(__dirname, '../workdir') + `/${file.originalFilename}`;
+    let filePath = path.join(WORKDIR) + `/${file.originalFilename}`;
     const upStream = fs.createWriteStream(filePath);
     reader.pipe(upStream);
     return result = {success: true,code: 200, message: "上传成功",file: file.originalFilename};
-        
+    })
+    ctx.body = res
+}
+// 执行文件
+let execFile = async (ctx, next) =>{
+   var {execFileName} = ctx.request.body
+    let res = await co(function* (){
+        var data = fs.readFileSync(`${WORKDIR}/${execFileName}`);
+                fs.writeFile(SWAGGERJSONFILE, data, function (err) {
+                    if (err) {
+                    ctx.throw({success: false,code: err.code, message: err.message}) }
+                })
+                return result={success: true,code: 200, message: "执行成功"}
     })
     ctx.body = res
 }
 
-module.exports = { checkConfigFileService,uploadFile }
+let deleteFile = async (ctx, next) =>{
+    var {deleteFileName} = ctx.request.body
+    if(deleteFile==''){
+        ctx.throw({success: false,code: 500, message: "参数不能为空"})
+    }
+    if(deleteFileName.split('.').pop()!='json'){
+        ctx.throw({success: false,code: 500, message: "参数不规范"})
+    }
+     let res = await co(function* (){
+        if(!fs.existsSync(`${WORKDIR}/${deleteFileName}`)){
+            ctx.throw({success: false,code: 500, message: "文件不存在"})
+        }
+        fs.unlink(`${WORKDIR}/${deleteFileName}`, function(err){
+            if (err) {
+                ctx.throw({success: false,code: err.code, message: err.message}) 
+            }
+        })
+        return result={success: true,code: 200, message: "删除成功"}
+     })
+     ctx.body = res
+ }
+
+
+let lookupConfig =async (ctx, next)=>{
+    var {fileName} = ctx.request.body
+    if(!fs.existsSync(`${WORKDIR}/${fileName}`)){
+        ctx.throw({success: false,code: 500, message: "文件不存在"})
+    }
+    let res = await co(function* (){
+        var data = fs.readFileSync(`${WORKDIR}/${fileName}`,'utf-8');
+        console.log(data)
+        return result = {success: true,code: 200, message: "读取成功",data: data}
+    })
+    ctx.body = res
+}
+
+module.exports = { checkConfigFileService,uploadFile ,execFile ,deleteFile,lookupConfig}
