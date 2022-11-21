42078ad4b74e51285a5fc60dc6df11efbbfcb2bb
林学文 <826982262@qq.com>
2022-11-21 13:09:12 +0800

    isforce-->force

--- service/swaggerService.js
+++ service/swaggerService.js
@@ -97,7 +97,7 @@ let putStorage = async (ctx, next) =>{
     console.log(swagger)
      let res = await co(function* (){
          var data = fs.readFileSync(`${WORKDIR}/swagger/${swagger}`);
-        let result=sendHttpRequest(`http://house.cloud.smallsaas.cn/api/u/lowAutoApis/syncJson?force=${isForce}`,data)
+        let result=sendHttpRequest(`http://house.cloud.smallsaas.cn/api/u/lowAutoApis/syncJson?force=${force}`,data)
 console.log(result)
      return result;
      })
